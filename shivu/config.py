0f92f75ad25a8cdc83a9c8144c4c3c59f7987e78
Akki1484 <gojo210606@gmail.com>
2025-02-09 18:38:32 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "1710597756"
-    sudo_users = [1710597756]
-    GROUP_ID = -1002483506913
+    OWNER_ID = "5282482434"
+    sudo_users = [5282482434]
+    GROUP_ID = -1002255658511
     TOKEN = "7586298589:AAHNpgN-iWCazr_yYR4XFANCRfpH4ILl4qg"
-    mongo_url = "mongodb+srv://vegetakun447:1jPSDznTX6gy7Nqr@cluster0.hcngy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    PHOTO_URL = ["http://ibb.co/WvZ6gLTr"]
-    SUPPORT_CHAT = "CollectYourLegends"
-    UPDATE_CHAT = "CollectYourLegends"
-    BOT_USERNAME = "LegendsDropBot"
-    CHARA_CHANNEL_ID = "-1002431614358"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    mongo_url = "mongodb://atlas-sql-67a756a7fe83b52654c45c5e-q1tei.a.query.mongodb.net/sample_mflix?ssl=true&authSource=admin"
+    PHOTO_URL = ["https://ibb.co/zhnpSBGR"]
+    SUPPORT_CHAT = "genshincatcher"
+    UPDATE_CHAT = "genshincatcher"
+    BOT_USERNAME = "GenshinC_bot"
+    CHARA_CHANNEL_ID = "-1002255658511"
+    api_id =  22085011
+    api_hash = "c098a7ff625c4a7c486cb6453aab0002"
 
     
 class Production(Config):
