54d4f2831b27c5c26b55c75e7e2ed540f7ed4296
Akki1484 <gojo210606@gmail.com>
2025-02-09 19:08:14 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,7 +5,7 @@ class Config(object):
     OWNER_ID = "5282482434"
     sudo_users = [5282482434]
     GROUP_ID = -1002255658511
-    TOKEN = "7586298589:AAHNpgN-iWCazr_yYR4XFANCRfpH4ILl4qg"
+    TOKEN = "7815890014:AAEkrcZOnvlv2PUU2A5IjEvxM7Xo3aYpwWo"
     mongo_url = "mongodb://atlas-sql-67a756a7fe83b52654c45c5e-q1tei.a.query.mongodb.net/sample_mflix?ssl=true&authSource=admin"
     PHOTO_URL = ["https://ibb.co/zhnpSBGR"]
     SUPPORT_CHAT = "genshincatcher"
